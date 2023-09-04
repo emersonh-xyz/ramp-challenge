8df85b0f938a4aa9c96ad94381bf462fc6b9aa00
Emerson <70560728+emersonh-xyz@users.noreply.github.com>
2023-09-04 12:28:00 -0400

    commit

---
+++ src/hooks/usePaginatedTransactions.ts
@@ -0,0 +1,41 @@
+import { useCallback, useState } from "react"
+import { PaginatedRequestParams, PaginatedResponse, Transaction } from "../utils/types"
+import { PaginatedTransactionsResult } from "./types"
+import { useCustomFetch } from "./useCustomFetch"
+
+export function usePaginatedTransactions(): PaginatedTransactionsResult {
+  const { fetchWithCache, loading } = useCustomFetch()
+  const [paginatedTransactions, setPaginatedTransactions] = useState<PaginatedResponse<
+    Transaction[]
+  > | null>(null)
+
+  const fetchAll = useCallback(async () => {
+
+    // If transactions are null set to empty array, otherwise set to current transactions data
+    const currentTransactions = paginatedTransactions ? paginatedTransactions.data : [];
+
+    const response = await fetchWithCache<PaginatedResponse<Transaction[]>, PaginatedRequestParams>(
+      "paginatedTransactions",
+      {
+        page: paginatedTransactions === null ? 0 : paginatedTransactions.nextPage,
+      }
+    )
+
+    setPaginatedTransactions((previousResponse) => {
+      if (response === null || previousResponse === null) {
+        return response
+      }
+
+      // Concat our current transactions with the next page of transactions
+      const allTransactions = currentTransactions.concat(response.data)
+
+      return { data: allTransactions, nextPage: response.nextPage }
+    })
+  }, [fetchWithCache, paginatedTransactions])
+
+  const invalidateData = useCallback(() => {
+    setPaginatedTransactions(null)
+  }, [])
+
+  return { data: paginatedTransactions, loading, fetchAll, invalidateData }
+}
